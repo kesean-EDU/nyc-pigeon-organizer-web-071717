37aac77ef4ab04b851682867349146bfc3c54b64
Kesean Woodhouse <kesean.woodhouse@gmail.com>
2017-07-12 01:47:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |attribute, values|
+    values.each do |stat, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_hash[pigeon] ||= {}
+        pigeon_hash[pigeon][attribute] ||= []
+        pigeon_hash[pigeon][attribute] << stat.to_s
+      end
+    end
+  end
+  return pigeon_hash
 end
